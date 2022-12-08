5f8a380ed3d6949c7998bcaff170b530c1e513aa
Tomie <taoming@hongwuniu.com>
2022-12-08 14:19:31 +0800

    修复函数调用错误的问题

--- testcases/test_websdk.py
+++ testcases/test_websdk.py
@@ -1,4 +1,6 @@
 # encoding: utf-8
+import json
+
 import allure
 from selenium import webdriver
 from pages.loginPage import LoginPage
@@ -17,14 +19,15 @@ logger = get_logger()
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 log_dir = os.path.join(os.path.dirname(cur_dir), "logs")
 img_dir = os.path.join(os.path.dirname(cur_dir), "screenshots")
+data_dir = os.path.join(os.path.dirname(cur_dir), "data")
 
 
 class TestWebSDK:
     def setup(self):
         driver = webdriver.Chrome()
         self.webSDK = WebSDKPage(driver)
-        user_id, user_token, env, auth_env = "3423224", "12313", "", ""
-        appKey, appSecert, other_params, enable_ssl = "", "", "", "use"
+        login_params = self.get_websdk_cfg()
+        user_id, user_token, env, auth_env, appKey, appSecert, other_params, enable_ssl = login_params.values()
         self.login(user_id, user_token, env, auth_env, appKey, appSecert, other_params, enable_ssl)
 
     def setup_class(self):
@@ -36,71 +39,144 @@ class TestWebSDK:
     def teardown_class(self):
         pass
 
+    def get_websdk_cfg(self):
+        web_sdk_cf = os.path.join(data_dir, "web_sdk.json")
+        with open(web_sdk_cf, "r") as f:
+            data = json.load(f)
+        return data
+
     @staticmethod
     def set_env_config(env):
-        add_env_cmd = """
-                        NSDK.addEnvType([
-                    {
-                        name: 'szse_dev',
-                        httpHostName : `dev-datasdk.mktdata.cn`,
-                        wsHostName : `dev-quotesdk.mktdata.cn`,
-                        customWsHostName: `dev-customize-sdk.mktdata.cn`,
-                        siteList : [
-                            "dev-shlistsdk.mktdata.cn/application/",
-                            "dev-gzlistsdk.mktdata.cn/application/"
-                        ]
-                    },
-                    {
-                        name: 'dev',
-                        httpHostName:`dev-platform.hongwuniu.com`,
-                        wsHostName:`dev-quote.hongwuniu.com`,
-                        customWsHostName:`dev-customize-sdk.hongwuniu.com`,
-                        siteList:[
-                            "dev-shlistsdk.hongwuniu.com/application/",
-                            "dev-gzlistsdk.hongwuniu.com/application/" 
-                        ]
-                    },
-                    {
-                        name: 'test',
-                        httpHostName:`test-platform.hongwuniu.com`,
-                        wsHostName:`test-quote.hongwuniu.com`,
-                        customWsHostName:`test-customize-sdk.hongwuniu.com`,
-                        siteList:[
-                            "test-shlistsdk.hongwuniu.com/application/",
-                            "test-gzlistsdk.hongwuniu.com/application/"
-                        ]
-                    },
-                    {
-                        name: 'uat',
-                        httpHostName:`uat-platform.hongwuniu.com`,
-                        wsHostName:`uat-quote.hongwuniu.com`,
-                        customWsHostName:`uat-customize-sdk.hongwuniu.com`,
-                        siteList:[
-                            "uat-shlistsdk.hongwuniu.com/application/",
-                            "uat-gzlistsdk.hongwuniu.com/application/"  
-                        ]
-                    },
-                    {
-                        name: 'pro',
-                        httpHostName : `platform.hongwuniu.com`,
-                        wsHostName : `quote.hongwuniu.com`,
-                        customWsHostName:`customize-sdk.hongwuniu.com`,
-                        siteList : [
-                            "shlistsdk.hongwuniu.com/application/",
-                            "gzlistsdk.hongwuniu.com/application/",
-                        ]
-                    }
-                ])
-                        """
-        which_env_cmd = f"NSDK.setEnvType('{env}');"
-        return [add_env_cmd, which_env_cmd]
+        envs = {
+            "szse_dev": {
+                "name": 'szse_dev',
+                "httpHostName": "dev-datasdk.mktdata.cn",
+                "wsHostName": "dev-quotesdk.mktdata.cn",
+                "customWsHostName": "dev-customize-sdk.mktdata.cn",
+                "siteList": [
+                    "dev-shlistsdk.mktdata.cn/application/",
+                    "dev-gzlistsdk.mktdata.cn/application/"
+                ]
+            },
+            "dev": {
+
+                "name": 'dev',
+                "httpHostName": "dev-platform.hongwuniu.com",
+                "wsHostName": "dev-quote.hongwuniu.com",
+                "customWsHostName": "dev-customize-sdk.hongwuniu.com",
+                "siteList": [
+                    "dev-shlistsdk.hongwuniu.com/application/",
+                    "dev-gzlistsdk.hongwuniu.com/application/"
+                ]
+            },
+            "test": {
+                "name": 'test',
+                "httpHostName": "test-platform.hongwuniu.com",
+                "wsHostName": "test-quote.hongwuniu.com",
+                "customWsHostName": "test-customize-sdk.hongwuniu.com",
+                "siteList": [
+                    "test-shlistsdk.hongwuniu.com/application/",
+                    "test-gzlistsdk.hongwuniu.com/application/"
+                ]
+            },
+            "uat": {
+                "name": 'uat',
+                "httpHostName": "uat-platform.hongwuniu.com",
+                "wsHostName": "uat-quote.hongwuniu.com",
+                "customWsHostName": "uat-customize-sdk.hongwuniu.com",
+                "siteList": [
+                    "uat-shlistsdk.hongwuniu.com/application/",
+                    "uat-gzlistsdk.hongwuniu.com/application/"
+                ]
+            },
+            "pro": {
+                "name": 'pro',
+                "httpHostName": "platform.hongwuniu.com",
+                "wsHostName": "quote.hongwuniu.com",
+                "customWsHostName": "customize-sdk.hongwuniu.com",
+                "siteList": [
+                    "shlistsdk.hongwuniu.com/application/",
+                    "gzlistsdk.hongwuniu.com/application/",
+                ]
+            },
+        }
+
+        add_env_cmd = f"NSDK.addEnvType({envs[env]});"
+        return add_env_cmd
+        # execjs.compile(add_env_cmd)
+        # execjs.compile(which_env)
+
+    @staticmethod
+    def set_env_config_old(env):
+        envs = """
+        [
+                {
+                    name: 'szse_dev',
+                    httpHostName : `dev-datasdk.mktdata.cn`,
+                    wsHostName : `dev-quotesdk.mktdata.cn`,
+                    customWsHostName: `dev-customize-sdk.mktdata.cn`,
+                    siteList : [
+                        "dev-shlistsdk.mktdata.cn/application/",
+                        "dev-gzlistsdk.mktdata.cn/application/"
+                    ]
+                },
+                {
+                    name: 'dev',
+                    httpHostName:`dev-platform.hongwuniu.com`,
+                    wsHostName:`dev-quote.hongwuniu.com`,
+                    customWsHostName:`dev-customize-sdk.hongwuniu.com`,
+                    siteList:[
+                        "dev-shlistsdk.hongwuniu.com/application/",
+                        "dev-gzlistsdk.hongwuniu.com/application/" 
+                    ]
+                },
+                {
+                    name: 'test',
+                    httpHostName:`test-platform.hongwuniu.com`,
+                    wsHostName:`test-quote.hongwuniu.com`,
+                    customWsHostName:`test-customize-sdk.hongwuniu.com`,
+                    siteList:[
+                        "test-shlistsdk.hongwuniu.com/application/",
+                        "test-gzlistsdk.hongwuniu.com/application/"
+                    ]
+                },
+                {
+                    name: 'uat',
+                    httpHostName:`uat-platform.hongwuniu.com`,
+                    wsHostName:`uat-quote.hongwuniu.com`,
+                    customWsHostName:`uat-customize-sdk.hongwuniu.com`,
+                    siteList:[
+                        "uat-shlistsdk.hongwuniu.com/application/",
+                        "uat-gzlistsdk.hongwuniu.com/application/"  
+                    ]
+                },
+                {
+                    name: 'pro',
+                    httpHostName : `platform.hongwuniu.com`,
+                    wsHostName : `quote.hongwuniu.com`,
+                    customWsHostName:`customize-sdk.hongwuniu.com`,
+                    siteList : [
+                        "shlistsdk.hongwuniu.com/application/",
+                        "gzlistsdk.hongwuniu.com/application/",
+                    ]
+                }
+            ]
+            
+        """
+
+        add_env_cmd = f"NSDK.addEnvType({envs});"
+        set_env_cmd = f"NSDK.setEnvType('{env}')"
+        return [add_env_cmd, set_env_cmd]
         # execjs.compile(add_env_cmd)
         # execjs.compile(which_env)
 
     def login(self, user_id, user_token, env, auth_env, appKey, appSecert, other_params, enable_ssl):
         websdk = self.webSDK
         websdk.open_env()
-        cmds = self.set_env_config("uat")
+        # cmds = self.set_env_config("uat")
+        # websdk.exec_js_cmd(cmds)
+
+        cmds = self.set_env_config_old("uat")
         for cmd in cmds:
             websdk.exec_js_cmd(cmd)
         websdk.connect_server()
